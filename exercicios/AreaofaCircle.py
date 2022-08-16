4e6bf88c86f20382c3651a42c347bb86dad8e4e2
unknown <florrafah94@edu.unifil.br>
2022-08-15 21:42:56 -0300

    exercicios2

--- exercicios/AreaofaCircle.py
+++
@@ -1,3 +0,0 @@
-r =float(input("Insira um numero para Calcular a área de circunferencia"))
-circu=3.14159*r**2
-print(f"A={circu}")
---
+++ exercicios/Simple_Sum.py
@@ -0,0 +1,4 @@
+A = int(input("Insira um numero"))
+B = int(input("insira o segundo numero"))
+SOMA=A+B
+print(f"SOMA = {SOMA}")
